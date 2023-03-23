ebc32b4256961bb2b044e4b1b43103b89dfe14f6
jinchangz <jinchangz1@gmail.com>
2023-03-24 00:07:27 +0900

    Fri Mar 24 00:07:27 JST 2023

---
+++ gitconfig.sh
@@ -0,0 +1,7 @@
+URL='git@github.com:jinchangz/next-sample.git'
+MAIL='jinchangz1@gmail.com'
+NAME='jinchangz'
+
+git config user.email $MAIL
+git config user.name $NAME
+git remote add origin $URL
--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,8 @@ URL='git@github.com:jinchangz/next-sample.git'
 # git remote add origin $URL
 # git branch -M main
 
+DATE=$(date)
+
 git add .
-git commit -m 
+git commit -m "${DATE}"
+git push -u origin main
